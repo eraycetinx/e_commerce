40ab633eeaf610f3a9cee0afbffefb89f251940b
ErayCetinX <cetin.eray@hotmail.com>
2026-02-10 13:15:18 +0300

    User route and controller are added

---
+++ backend/src/controller/user.controller.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+
+export const updateProfile = async (req: Request, res: Response) => {};
+export const profile = async (req: Request, res: Response) => {};
---
+++ backend/src/route/user.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import * as userController from "../controller/user.controller";
+
+const router = express.Router();
+
+router.put("/update-profile", userController.updateProfile);
+router.get("/profile/:username", userController.profile);
